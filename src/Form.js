9eb425995b29811754cfcd45a9fe7c9fc94b3f44
Ashley Heestand <heestand.ashley@gmail.com>
2019-08-02 11:10:53 -0700

    check type

--- src/Form.js
+++ src/Form.js
@@ -13,43 +13,90 @@ class Form extends React.Component {
       validated: false,
     };
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.checkType = this.checkType.bind(this);
+    // this.checkType = this.checkType.bind(this);
+    this.validate = this.validate.bind(this);
   }
 
+  //Need to change the type when a new number is input
   handleInputChange(event) {
+    // console.log('event', event.target.value.charAt(0));
     const target = event.target;
     const value = target.value;
     const name = target.name;
+    if (name === 'number') {
+      const firstNumber = event.target.value.charAt(0);
+      const secondNumber = event.target.value.charAt(1);
+      // console.log(firstNumber, 'first');
+      if (firstNumber === '4') {
+        this.setState({ type: 'Visa' });
+      } else if (firstNumber === '5') {
+        this.setState({ type: 'Mastercard' });
+      } else if (
+        firstNumber === '3' &&
+        (secondNumber === '3' || secondNumber === '7')
+      ) {
+        this.setState({ type: 'American Express' });
+      }
+    }
     this.setState({ [name]: value });
     if (this.state.number) {
-      this.checkType();
+      // this.checkType();
+      this.validate();
     }
   }
 
-  checkType() {
-    if (this.state.number.slice(0) === 4) {
-      this.setState({ type: 'Visa' });
+  //also need a handle submit function
+  //should move checking for type out of the handle change function
+
+  // checkType() {
+  //   console.log('character', this.state.number.charAt(0));
+  //   if (this.state.number.charAt(0) === '4') {
+  //     this.setState({ type: 'Visa' });
+  //   }
+  //   if (this.state.number.charAt(0) === 5) {
+  //     this.setState({ type: 'Mastercard' });
+  //   }
+  //   if (
+  //     this.state.number.charAt(0) === 3 &&
+  //     (this.state.number.charAt(1) === 4 || this.state.number.charAt(1) === 7)
+  //   ) {
+  //     this.setState({ type: 'American Express' });
+  //   } else {
+  //     this.setState({ type: '?' });
+  //   }
+  // }
+
+  //not currently using this function -- there is definitely a better way to validate using react forms
+  validate() {
+    if (
+      this.state.type === 'Visa' &&
+      this.state.number.length >= 13 &&
+      this.state.number.length <= 16
+    ) {
+      this.setState({ validated: true });
     }
-    if (this.state.number.charAt(0) === 5) {
-      this.setState({ type: 'Mastercard' });
+    if (this.state.type === 'Mastercard' && this.state.number.length === 16) {
+      this.setState({ validated: true });
     }
     if (
-      this.state.number.charAt(0) === 3 &&
-      (this.state.number.charAt(1) === 4 || this.state.number.charAt(1) === 7)
+      this.state.type === 'American Express' &&
+      this.state.number.length === 15
     ) {
-      this.setState({ type: 'American Express' });
+      this.setState({ validated: true });
     } else {
-      this.setState({ type: '?' });
+      this.setState({ validated: false });
     }
   }
 
   render() {
     console.log('state', this.state);
-    console.log('char', this.state.number.slice(0));
+    console.log('char', this.state.number.charAt(0));
     return (
       <div>
         <h1>Order Now</h1>
         <form>
+          {/* need to fix where this appears and styling  */}
+          <div>Type: {this.state.type}</div>
           <label>
             Credit card number:
             <input
@@ -59,7 +106,7 @@ class Form extends React.Component {
               onChange={this.handleInputChange}
             />
           </label>
-          <div>Type: {this.state.type}</div>
+
           <label>
             Name on card:
             <input
